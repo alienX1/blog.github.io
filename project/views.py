2c0695706d64ce7209e826090852a5d6a6d0728f
AlienX <aditya1115.ar@gmail.com>
2021-10-29 10:16:24 +0530

    my blog website

---
+++ project/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'posts.html', {'posts': posts})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credential invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def register(request):
+    return render(request, 'register.html')
